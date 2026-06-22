c14bdb92ad4755aebc47476fc3dc4b195facdc05
thatiswat <vishalsrao2004@gmail.com>
2026-06-22 17:10:13 +0530

    Update stt.py

--- backend/app/services/stt.py
+++ backend/app/services/stt.py
@@ -1,24 +1,24 @@
+import os
 from groq import Groq
-from app.config import GROQ_API_KEY
 
-client = Groq(
-    api_key=GROQ_API_KEY
-)
 
-async def speech_to_text(
-    audio_path: str
-):
+def get_client():
+    api_key = os.getenv("GROQ_API_KEY")
 
-    with open(
-        audio_path,
-        "rb"
-    ) as audio_file:
+    if not api_key:
+        raise Exception("GROQ_API_KEY is missing")
 
-        transcription = (
-            client.audio.transcriptions.create(
-                file=audio_file,
-                model="whisper-large-v3"
-            )
+    return Groq(api_key=api_key)
+
+
+async def speech_to_text(audio_path: str):
+
+    client = get_client()
+
+    with open(audio_path, "rb") as audio_file:
+        transcription = client.audio.transcriptions.create(
+            file=audio_file,
+            model="whisper-large-v3"
         )
 
     return transcription.text
