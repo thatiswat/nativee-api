029dfc3e6473ae53b76c88669886f1f8b4d4f6ac
Vishal <vishal@Vishals-MacBook-Pro.local>
2026-06-26 20:16:44 +0530

    Measure ttt latency

--- app/services/translator.py
+++ app/services/translator.py
@@ -1,11 +1,5 @@
+from deep_translator import GoogleTranslator
 import time
-
-from groq import Groq
-from app.config import GROQ_API_KEY
-
-client = Groq(
-    api_key=GROQ_API_KEY
-)
 
 
 async def translate_text(
@@ -15,43 +9,13 @@ async def translate_text(
 ):
     start = time.perf_counter()
 
-    prompt = f"""
-Translate the following text.
-
-Rules:
-- Translate ONLY.
-- Do not explain.
-- Do not add quotation marks.
-- Preserve names.
-- Preserve punctuation.
-- Output only the translated sentence.
-
-Source Language: {source_language}
-Target Language: {target_language}
-
-Text:
-{text}
-"""
-
-    response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        temperature=0,
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-    )
-
-    translated = (
-        response.choices[0]
-        .message.content
-        .strip()
-    )
+    translated = GoogleTranslator(
+        source="auto",
+        target=target_language,
+    ).translate(text)
 
     print(
-        f"🌍 Groq Translation: {time.perf_counter()-start:.3f}s"
+        f"🌍 Google Translation: {time.perf_counter() - start:.3f}s"
     )
 
     return translated
